f1942fdefb3b1ba506106eae4c2d8272fe41a691
janghyejin0 <48415608+janghyejin0@users.noreply.github.com>
2020-09-13 13:25:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,20 +5,16 @@ function startConverting ()
         //check this browser is chrome or not. because this application supported only in chrome browser
 
         if('webkitSpeechRecognition'in window){
-            //Web speech API Function
             var speechRecognizer = new webkitSpeechRecognition();
-            //continuous : you will catch mic only one time or not
             speechRecognizer.continuous = true;
             //interimResults : during capturing the mic you will send results or not
             speechRecognizer.interimResults = true;
-            //lang : language (ko-KR : Korean, en-IN : englist)
             speechRecognizer.lang="ko-KR";
-            //start!
+
             speechRecognizer.start();
 
             var finalTranscripts = '';
 
-            //if the voice catched onresult function will start
             speechRecognizer.onresult=function(event){
                 var interimTranscripts='';
                 for(var i=event.resultIndex; i < event.results.length; i++)
@@ -35,13 +31,12 @@ function startConverting ()
                     }
                 }
                 //insert into HTML
-                r.innerHTML=finalTranscripts+'<span style="color:#999">'+interimTranscripts+'</span>';
+                r.innerHTML=finalTranscripts+'<div style="color:#999">'+interimTranscripts+'</div>';
             };
             speechRecognizer.onerror = function(event){
             };
         }
         else{
-            //if browser don't support this function. this message will show in your web
-            r.innerHTML ="your browser is not supported. If google chrome. Please upgrade!";
+            r.innerHTML ="크롬에서만 가능합니다! 크롬으로 접속해주세요.";
         }
     }
