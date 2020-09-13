ddcd27c9ba8d8205813514390479643fab6f9548
janghyejin0 <48415608+janghyejin0@users.noreply.github.com>
2020-09-13 16:15:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var r=document.getElementById('result');
 
 function startConverting ()
@@ -6,10 +5,13 @@ function startConverting ()
         //check this browser is chrome or not. because this application supported only in chrome browser
 
         if('webkitSpeechRecognition'in window){
+            //Web speech API Function
             var speechRecognizer = new webkitSpeechRecognition();
+            //continuous : you will catch mic only one time or not
             speechRecognizer.continuous = true;
             //interimResults : during capturing the mic you will send results or not
             speechRecognizer.interimResults = true;
+            //lang : language (ko-KR : Korean, en-IN : englist)
             speechRecognizer.lang="ko-KR";
 
             speechRecognizer.start();
